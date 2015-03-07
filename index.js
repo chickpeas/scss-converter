342a2f6a9c694c38e37a82b1639e316b5a7e993a
chickpeas <cecilia@Cecilias-MacBook-Air.local>
2015-03-07 12:21:40 +0000

    require parse-sass lib

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
 var sass = require('./sassify');
+var sassParser = require('./lib/parse-sass.js');
 
-var cssfile = fs.readFileSync(process.argv[2], 'utf-8');
 
-sass.sassifyCss(cssfile);
+var sassObj, cssfile = fs.readFileSync(process.argv[2], 'utf-8');
+
+var variableFile; 
+variableFile = process.argv[3] ? fs.readFileSync(process.argv[3], 'utf-8') : false;
+if (variableFile) {sassObj = sassParser(variableFile);}
+sass.sassifyCss(cssfile, sassObj);
